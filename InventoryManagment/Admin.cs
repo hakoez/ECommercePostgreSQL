2d8117064a783236413ee19c46cd67f16d0f4d9d
hakan özdemir <101719280+hakoez@users.noreply.github.com>
2025-07-28 10:29:38 +0200

    Update Admin.cs

--- InventoryManagment/Admin.cs
+++ InventoryManagment/Admin.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +8,6 @@ namespace InventoryManagment
 {
     public class Admin : User
     {
-        
+        //eklenebilecekler satis raporlari , kullanici bilgi degisiklikleri
     }
 }
